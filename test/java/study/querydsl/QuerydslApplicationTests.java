7ad3dcadad5b4adbbb60d87662d6aadd9730bbc4
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-02-09 00:11:22 +0900

    섹션1. 프로젝트 환경설정

---
+++ test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,43 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional	// 테스트에 @Transactional이 있으면 기본적으로 테이블을 ROLLBACK
+@Commit		// 공부할 땐 @Commit 옵션으로 테이블을 눈으로 확인하자
+class QuerydslApplicationTests {
+
+//	@PersistenceContext // @Autowired와 동일한 기능
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+}
